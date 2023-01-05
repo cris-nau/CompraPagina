13a58b79bb434b1c4acca63c3dc2b738ed34ae3a
cris-nau <naulasantiago537@gmail.com>
2023-01-05 12:03:55 -0500

    Productos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { Componente1Component } from './componente1/componente1.component';
 import { Componente2Component } from './componente2/componente2.component';
 import { Componente3Component } from './componente3/componente3.component';
+import { Componente4Component } from './componente4/componente4.component';
 
 
 const routes: Routes = [
@@ -14,6 +15,9 @@ const routes: Routes = [
   },
   { 
     path: "register", component: Componente3Component, pathMatch: "full" 
+  },
+  {
+    path: "carrito", component: Componente4Component, pathMatch: "full" 
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { Componente1Component } from './componente1/componente1.component';
 import { Componente2Component } from './componente2/componente2.component';
 import { Componente3Component } from './componente3/componente3.component';
 import { FormsModule } from '@angular/forms';
+import { Componente4Component } from './componente4/componente4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Componente1Component,
     Componente2Component,
-    Componente3Component
+    Componente3Component,
+    Componente4Component
   ],
   imports: [
     BrowserModule,
